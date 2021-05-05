75e2cbadf7ff83a1bb8f585a6e635e1e701e77a3
Matheus Fugisaki <matheusgfmaciel@gmail.com>
2021-05-05 20:07:51 -0300

    first commit

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import streamThumbnail from '../../images/stream_thumbnail.jpg';
+
+import {
+  List,
+  StreamContainer,
+  StreamThumbnail,
+  StreamColumn,
+  StreamRow,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from './styles';
+
+const StreamList: React.FC = () => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>fugisaki_official</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Jogando Among Us até altas horas
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>
+            Consoles & Games
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Among Us</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  )
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+}
+
+export default StreamList;
